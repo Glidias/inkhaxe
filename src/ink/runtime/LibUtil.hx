67fd77eef6fea90431a8ba0078b762ac88a52a39
Glidias <glidias@hotmail.com>
2016-09-07 19:59:11 +0800

    typo missing !

--- src/ink/runtime/LibUtil.hx
+++ src/ink/runtime/LibUtil.hx
@@ -69,7 +69,7 @@ class LibUtil
 	public static function arraySequenceEquals<T:IEquatable<T>>(arr1:Array<T>, arr2:Array<T>):Bool {
 		if (arr1.length != arr2.length) return false;
 		for (i in 0...arr1.length) {
-			if (arr1[i].Equals(arr2[i])) {  // enforced IEquatable constraint
+			if (!arr1[i].Equals(arr2[i])) {  // enforced IEquatable constraint
 				return false;
 			}
 		}
