fdbd70c6bdb466d509a116331fa9357ae12e057d
Glenn Ko <glidias@hotmail.com>
2016-09-12 02:40:41 +0800

    removed depreciated SimpleJson

--- src/InkleRuntime.hx
+++ src/InkleRuntime.hx
@@ -21,68 +21,11 @@ class InkleRuntime
 		Value;
 		VariablesState;
 		NativeFunctionCall;
-		SimpleJson;
+	
 		StoryState;
 		Story;
 		Path;
-		//trace("A");
 		
-		//testDataTypeClasses();
-		//testCommandTypeEnum();
-	}
-	
-	static private function testCommandTypeEnum():Void
-	{
-		var arrTest = ["index0", "index1"];
-		trace( arrTest[cast CommandType.EvalStart]);
-		var cmdType:Int = cast CommandType.EvalStart;
-		trace(cmdType);
-		var cmdTypeE:CommandType = CommandType.EvalStart;
-		trace(Std.string(cmdTypeE));
-		
-		var map:Map<String, Object> = new Map<String, Object>();
-		var mapInt:Map<Int, Object> = new Map<Int, Object>();
-		var mapSet:HashSetString = new HashSetString();
-		
-		var dynStrMap:Map<Container, Dynamic> = new  Map<Container,Dynamic>();
-		
-		trace("Std is dynamic:" + Std.is(dynStrMap,ObjectMap) + ", "+Std.is({}, StringMap));
-		
-		mapSet.add("abc");
-		
-		var strMapBool:StringMap<Bool> = new StringMap<Bool>();
-		
-	
-		
-		map.set("abc", new Object());
-		mapInt.set(1, map.get("abc"));
-		var json:Dynamic = { "abc":map.get("abc") };
-		//trace(map.get("abc") == LibUtil.tryGetValue(mapInt, 1));
-		trace(LibUtil.tryGetValue(map, "abc") == LibUtil.tryGetValueDynamic(json, "abc"));
-		
-		
-	}
-
-	
-	
-	private static function testDataTypeClasses():Void {
-		trace(Type.getClass( Value.Create("abc") ) );
-		trace( Type.getClass(Value.Create("1")) );
-		trace( Type.getClass(Value.Create("0")) );
-		trace( Type.getClass(Value.Create("1.0")) );
-		trace( Type.getClass(Value.Create("0.01")) );
-		trace( Type.getClass(Value.Create(1)) );
-		trace( Type.getClass(Value.Create(0)) );
-		trace( Type.getClass(Value.Create(1.0)) );
-		trace( Type.getClass(Value.Create(0.01)) );
-		trace( Type.getClass(Value.Create(true)) );
-		trace( Type.getClass(Value.Create(false)) );
-		trace(CommandType.PopEvaluatedValue);
-		trace(  Value.Create("1").isTruthy );
-		
-	
-		//new Json();
-		//trace(Json._controlCommandNames);
 	}
 	
 	
